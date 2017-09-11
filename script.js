85bbcc3bc25be19b473e4dfaa5ddc1cff501bf83
yellow544 <31159133+yellow544@users.noreply.github.com>
2017-09-11 20:08:42 +0100

    ambitious update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function drawTree(h) {
   for(var i = 0; i < h; i++) {
     var star = " ";
     for(var j = 0; j <= i; j++){
-      star += '*';
+      star += '*' + ' ';
     }
     console.log(star);
   }
