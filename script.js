368c8b77af64d7168d1eaa137c7b8781df7ba8a8
yellow544 <31159133+yellow544@users.noreply.github.com>
2017-09-13 22:30:45 +0100

    proper christmas tree made

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function drawTree(h) {
-  for(var i = 0; i < h; i++) {
-    var star = " ";
-    for(var j = 0; j <= i; j++){
-      star += '*' + ' ';
+  for (var i = 0; i < h; i++) {
+    var star = '';
+    
+    for (var j = 0; j <= i; j++) {
+      star += '* ';  
     }
-    console.log(star);
-  }
-  
+    
+    var space = '';
+    for (var k = h; k > i; k--) {
+      space += ' ';
+    }
+      
+    console.log(space + star);
+  } 
 }
 
 drawTree(10);
-
 
